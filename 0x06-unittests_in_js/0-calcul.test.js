d5e92c97484404090b379938556e98d32e31ceac
Nftalem Arega <robelpopandro@gmail.com>
2023-05-18 17:06:35 -0200

    Solve: Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('calculated', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('calculated', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('calculated', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('calculated', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
